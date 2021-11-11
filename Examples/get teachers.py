9bca0de03e1601c072a44a620c7f50ddd6aa5f96
Yaurrdautia <63754863+Yaurrdautia@users.noreply.github.com>
2021-11-10 19:52:30 -0800

    Add files via upload

---
+++ Examples/get teachers.py
@@ -0,0 +1,15 @@
+import skywrd
+
+#initializes the library with the parameters.
+skywrd.setup("uratayut000","Ryzen700@o")
+
+#logs into account and grabs the string that has the session id in it.
+session = skywrd.get_login()
+
+#gets the html for the whole page.
+html = skywrd.get_gradbook_html(session)
+
+#gets the names of the teachers and cources as a list.
+teachers = skywrd.get_teachers(html)
+
+print(teachers)
